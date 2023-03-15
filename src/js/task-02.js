05874c7ebcdc5cedfe0bd46ea2f87695bfebd28c
Soundlover1984 <denys.kovtun@gmail.com>
2023-03-15 22:54:28 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRoster = document.querySelector('#ingredients');
+const ingredientsElements = ingredients.map((ingredient) =>{
+  const listElement = document.createElement('li');
+  listElement.textContent = ingredient;
+  listElement.classList.add('item');
+  return listElement;
+})
+
+ingredientsRoster.append(...ingredientsElements);
